6a7da20fdf9b122a8460a9adda5c4f51aba207df
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-11-19 20:35:37 +0600

    Create Page00.c

---
+++ Integrated_Project/Page00.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	printf("\n\n\t\t\t\t\t\t\t\t\t\tWelcome to\n");
+	printf("\t\t\t\t\t\t\t\t\t\t----------LOST KITCHEN-------------\n");
+	printf("\t\t\t\t\t\t\t\t\t\t\t-Where every flavor tells a story\n\n\n");
+	
+	printf("1. About us\n");
+	printf("2. User guidelines\n");
+	printf("3. Authentication system\n");
+	printf("4. Order now!\n");
+	printf("5. Hot deals!!!\n");
+	printf("6. FAQs\n");
+	printf("7. Review\n");
+	printf("8. Exit\n");
+
+	return 0;
+}

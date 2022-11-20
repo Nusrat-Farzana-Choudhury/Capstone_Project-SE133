91299477563c60a755fb7927426a17ac7a5a157a
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-11-20 23:49:50 +0600

    Create Page01.c

---
+++ Integrated_Project/Page01.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	 printf("\n\n\t\t\t\t\t\t\t\t\t\t\t---------------------About Us---------------------\n\n");
+	 printf("----------Our story----------\n");
+	 printf("Our system is specially made for resident students of Daffodil International University who live in Ashuliya.\nIn our university, a large number of students come from far away.They stay in the hall or live on rent nearby.\nMany of them eat at different restaurants daily.\nWhile having food they face some challenges regularly such as consuming unhealthy foods, having a poor diet, safety issues for females, and unreasonable costs.\n\t-These thoughts led us to create a homemade food delivery service named 'LOST KITCHEN'.\n\tIt will bring back the taste, which they used to get from the kitchen of their home right next to their doorsteps within a few clicks.\n\n");
+	 printf("----------Our Mission----------\n");
+	 printf("We are committed to providing you with-\n\t~Hygenic food made with care\n\t~Food made with fresh ingredients\n\t~Fast delivery with no delivery cost at all\n\t~Offer packages maintaining proper diet chart by a certified nutritionist\n\n");
+	 printf("***OUR SERVICES ARE ONLY AVAILABLE IN ASHULIYA & JUST FOR THE STUDENT OF DAFFODIL INTERNATIONAL UNIVERSITY.***\n\n");
+	 printf("----------Our team----------\n");
+	 printf("Our team is consisted of a group of courageous people who have made our service successful-\n\n\t||Founder:\n\t\t~Ms. Nusrat Farzana Choudhury\n\t\t Founder, Lost Kitchen\n\t\t~Ms. Mashruba Sultana Zerin\n\t\t Founder, Lost Kitchen\n\n\t||Management Team:\n\t\t~Ms. Dr. Tasnim Zara\n\t\t Nutritionist, Lost Kitchen\n\t\t~Mr. Abin Mahmud Khan\n\t\t Manager, Lost Kitchen\n\t\t~Mr. Jamshed Uddin\n\t\t Supervisor, Lost Kitchen\n\t\t~Ms. Sheuly Khanam\n\t\t Customer support, Lost Kitchen\n\n\t||Delivery service:\n\t\t~SteadFast\n\n");
+	 printf("----------Contact Us----------\n");
+	 printf("Address-\n18/A, Prantik Road,\nCharabagh, Ashuliya,\nDhaka.\n\nPhone number-\n\t~ +88 01423272703\n\t~ +02 436183543\n\nE-mail:\nlostkitchensupport@gmail.com\n\n");
+	
+	/*int opt_1; 
+	  printf("Press 1 to back to homepage or 0 to exit"); 
+	  scanf("%d",&opt_1);
+	 if (opt_1==1)
+	  home_page(); //recalling home_page (code-page00) func 
+   if (opt_1==0)
+    return 0;
+	*/
+	
+	return 0;
+}

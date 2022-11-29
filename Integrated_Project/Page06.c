0f4c39ed03936981e6b7d773a4ac99f9235e30ac
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-11-29 16:04:22 +0600

    FAQs

---
+++ Integrated_Project/Page06.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	
+	printf("\t\t\t\t\t\t\t\t\t\t\t----------FAQs-------------\n");
+	printf("\t\t\t\t\t\t\t\t\t\t\t\t--Solutions to make things easier\n\n\n");
+	printf("---Where are you situated?---\n\tWe are situated in Ashuliya. You can find more info in 'About Us' page.\n\n");
+	printf("---Do you deliver outside Ashuliya?---\n\tNo.\n\n");
+	printf("---How much is your delivery charge?---\n\t0 BDT. Yes, you read it right, we deliver free of cost for students of Daffodil International University.\n\n");
+    printf("---Why does it always show different offers at the same time for different users?---\n\tIt's somewhat like a lottery! It depends on your luck. But you'll get at least 1 offer for sure.\n\n");
+    printf("---Can I make offline purchase directly from your address?---\n\tYes.\n\n");
+    printf("---What are your time for offline purchase hours?---\n\tFrom 10 am-7 pm,Saturday to Friday\n\n");
+    printf("---Can I get discount on offline purchase?---\n\tNo.\n\n");
+    printf("---Why is it only for DIU students?---\n\tActually, we try to provide our best services to our customers, so it initially started in a small group.\n\tStill, as we expand our branches we'll try to provide for all customers in Ashuliya or nearby.\n\n");
+    printf("---How to place an order?---\n\tSo easy!!! Just check out 'User Guides'.\n\tIf you need further help, please feel free to contact us.\n\n");
+    printf("---What is the approximate delivery time?---\n\t20 minutes.\n\n");
+	printf("---I have encountered a bug or the system is not working---\n\tPlease send us an e-mail at lostkitchensupport@gmail.com along with screenshots of the problem with a detailed description.\n\tWe'll fix it for you ASAP.\n\n");
+	printf("---My question is not listed here---\n\tIf you cannot find your query, please feel free to contact us:\n\n\tE-mail:\n\tlostkitchensupport@gmail.com\n\n\tPhone number:\n\t~ +88 014232727033\n\t~ +02 436183543\n\n");
+	return 0;
+}
